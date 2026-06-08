ff897a4b13784fb223247d26b135a3444e75bec9
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2026-06-08 20:01:50 +0530

    initialized youtube manager project

---
+++ Youtube_manager.py
@@ -0,0 +1,61 @@
+import json
+
+fileName = 'youtube.txt'
+
+def load_data():
+    try:
+        with open(fileName, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+    
+def save_data_helper(videos):
+    with open(fileName, 'w') as file:
+        json.dump(videos, file)
+
+def list_all_videos(videos):
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}. ")
+
+def add_video(videos):
+    name = input("Enter video name: ")
+    time = input("Enter video time: ")
+    videos.append({'name': name, 'time': time})
+    save_data_helper(videos)
+
+def update_video(videos):
+    pass
+
+def delete_video(videos):
+    pass
+
+
+def main():
+    videos = load_data()
+    while True:
+        print("\n Youtube Manager | choose an option  ")
+        print("1. List all youtube videos ")
+        print("2. Add a youtube video")
+        print("3. Update a youtuve video details")
+        print("4. Delete a youtube video")
+        print("5. Exit the app")
+        choice = input("Enter your choice: ")
+        print(videos)
+
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                update_video(videos)
+            case '4':
+                delete_video(videos)
+            case '5':
+                break
+            case _:
+                print("Invalid choice")
+            
+
+if __name__ == "__main__":
+    main()
