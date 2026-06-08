f9c2bab44063b529710921ce6532dac79ed4da8e
Nitin Chakradhari <154994765+nkchakradhari780@users.noreply.github.com>
2026-06-08 20:19:50 +0530

    implemented update and delete method

--- Youtube_manager.py
+++ Youtube_manager.py
@@ -16,10 +16,11 @@ def save_data_helper(videos):
         json.dump(videos, file)
 
 def list_all_videos(videos):
+    print("\n")
+    print("*" * 70)
     for index, video in enumerate(videos, start=1):
-        print("\n")
-        print("*" * 70)
         print(f"{index}. {video['name']}, Duration: {video['time']}")
+    print("*" * 70)
 
 def add_video(videos):
     name = input("Enter video name: ")
@@ -28,10 +29,24 @@ def add_video(videos):
     save_data_helper(videos)
 
 def update_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to update"))
+    if 1 <= index <= len(videos):
+        name = input("Enter the new video name: ")
+        time = input("Enter the new video time: ")
+        videos[index-1] = {'name': name, 'time': time}
+        save_data_helper(videos)
+    else: 
+        print("invalid index selected")
 
 def delete_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to delte"))
+    if 1 <= index <= len(videos):
+        del videos[index-1]
+        save_data_helper(videos)
+    else: 
+        print("invalid index selected")
 
 
 def main():
